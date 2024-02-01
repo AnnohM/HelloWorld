b324ec1d93d2d11147dd94636639b3dbf88b597b
annoh <amienza@terpmail.umd.edu>
2024-01-31 21:04:55 -0500

    Changed "H" in hello from uppercase to lowercase

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from Annoh Mienza! ' 'I am adding my first code change.'
+    return 'Hello World from Annoh Mienza! ' 'This is my first HTML page.'
 
 app.route ('/hello')
 def hello ():
-    return render_template('Hello.html')
+    return render_template('hello.html')
 if __name__ == '__main__':
     app.run()
