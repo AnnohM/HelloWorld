caa97c2d7c70189893a78030980a1cd4a238406a
annoh <amienza@terpmail.umd.edu>
2024-02-29 13:38:07 -0500

    testing to see why my about page won't work

--- app.py
+++ app.py
@@ -8,13 +8,22 @@ def hello_world():  # put application's code here
     return 'Hello World from Annoh Mienza! ' 'This is my first HTML page.'
 
 @app.route ('/hello')
-def hello ():
+def hello():
     return render_template('hello.html')
 if __name__ == '__main__':
     app.run()
 
 @app.route ('/about')
-def about ():
+def about():
     return render_template('about.html')
 if __name__ == '__main__':
     app.run()
+
+
+    @app.route('/teamabout')
+    def about():
+        return render_template('teamabout.html')
+
+
+    if __name__ == '__main__':
+        app.run()
