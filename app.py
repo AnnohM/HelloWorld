959a996058c27eefa220138e5a693228b6391b28
annoh <amienza@terpmail.umd.edu>
2024-02-29 13:48:35 -0500

    added an about branch

--- app.py
+++ app.py
@@ -7,23 +7,21 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World from Annoh Mienza! ' 'This is my first HTML page.'
 
-@app.route ('/hello')
+
+@app.route('/hello')
 def hello():
     return render_template('hello.html')
-if __name__ == '__main__':
-    app.run()
 
-@app.route ('/about')
+
+@app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/teamabout')
+def teamabout():
+    return render_template('teamabout.html')
+
+
 if __name__ == '__main__':
     app.run()
-
-
-    @app.route('/teamabout')
-    def about():
-        return render_template('teamabout.html')
-
-
-    if __name__ == '__main__':
-        app.run()
